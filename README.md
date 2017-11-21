177f71ce548751faa4609629b75e7fb321326332
Matt Lindsay <matt_epilp@hotmail.com>
2017-11-21 15:27:40 +0000

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Box nodejs SDK example
+
+Using an expressjs application, authenticate and access the Box API using Box's nodejs SDK.
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// This example uses an expressjs web app to access the Box nodejs SDK.
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var Box = require('box-node-sdk');
@@ -7,6 +9,7 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Box app secrets stored as environment variables.
 var boxClientID = process.env.boxClientID;
 var boxClientSecret = process.env.boxClientSecret;
 var privateKey = process.env.boxPrivateKey;
@@ -15,6 +18,7 @@ var publicKeyPassphrase = process.env.boxKeyPassphrase;
 var boxEnterpriseId = process.env.boxEnterpriseId;
 var boxUser = process.env.boxUser;
 
+// Set the sdk up - https://github.com/box/box-node-sdk/#app-auth-client
 var sdk = new Box({
     clientID: boxClientID,
     clientSecret: boxClientSecret,
@@ -25,18 +29,21 @@ var sdk = new Box({
     }
 });
 
+// Create a client.
 var client = sdk.getAppAuthClient('enterprise', boxEnterpriseId);
+// Set the managed user to impersonate - https://developer.box.com/reference/as-user#as-user-1
 client.asUser(boxUser);
 
+// Create an index route for the express application.
 app.use('/', function (req, res) {
+    // Execute a Box API call when the index route is requested.
     client.users.get(client.CURRENT_USER_ID, null, function (err, userResponse) {
       if (err) {
           res.send(err);
       } else {
-          res.send(userResponse);
+          res.send(userResponse); // This returns the raw JSON from Box's API.
       }
     });
-    //res.send('Hello');
 });
 
 app.listen(port, function (err) {
